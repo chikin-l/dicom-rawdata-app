813cfcc893f890e361ca30bbd38d994430f775be
Chi Kin <114360748+chikin-l@users.noreply.github.com>
2026-06-17 13:05:17 +0100

    Update

--- dicom_rawdata_listmode.sh
+++ dicom_rawdata_listmode.sh
@@ -12,6 +12,7 @@ FILE="$1"
 docker compose run --rm \
   -v "$(dirname "$FILE"):/input" \
   dicom_rawdata_listmode \
-  python dicom_rawdata_listmode.py -fp "/input/$(basename "$FILE")"
+  python dicom_rawdata_listmode.py -fp "/input/$(basename "$FILE")" \
+  > >(grep -v '\[WARNING\]') 2> >(grep -Ev 'Container .* (Creating|Created)|\[WARNING\]' >&2)
 
 exit 0
